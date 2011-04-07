42e96d4466993b67efc7129b1cf5d9813b3c7ce6
Josh Nussbaum <joshnuss@gmail.com>
2011-04-07 12:04:41 -0400

    added create action to stock request

--- app/controllers/stock_requests_controller.rb
+++ app/controllers/stock_requests_controller.rb
@@ -2,4 +2,14 @@ class StockRequestsController < Spree::BaseController
   def new
     @stock_request = StockRequest.new
   end
+
+  def create
+    @stock_request = StockRequest.create(params[:stock_request])
+
+    if @stock_request.valid?
+      redirect_to root_path, :notice => t(:successful_stock_request)
+    else
+      render :action => 'new'
+    end
+  end
 end
--- spec/controllers/stock_requests_controller_spec.rb
+++ spec/controllers/stock_requests_controller_spec.rb
@@ -12,5 +12,29 @@ describe StockRequestsController do
   end
 
   context "POST /create" do
+    context "valid data" do
+      let(:request) { mock_model(StockRequest, :valid? => true) }
+
+      before do
+        StockRequest.should_receive(:create).with(:data).and_return(request)
+        post :create, :stock_request => :data
+      end
+
+      specify { response.should redirect_to(root_path) }
+      specify { flash[:notice].should == I18n.t(:successful_stock_request) }
+    end
+
+    context "invalid data" do
+      let(:request) { mock_model(StockRequest, :valid? => false) }
+
+      before do
+        StockRequest.should_receive(:create).with(:data).and_return(request)
+        post :create, :stock_request => :data
+      end
+  
+      specify { assigns(:stock_request).should == request }
+      specify { response.should be_success }
+      specify { response.should render_template('new') }
+    end
   end
 end
