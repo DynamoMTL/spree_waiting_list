3474ed2db219d1a156a1d7cba3c08e73935e4242
Diego Domingues <diego.domingues16@gmail.com>
2014-12-15 10:16:18 -0200

    Fixes ransack param name

--- app/controllers/spree/admin/reports_controller_decorator.rb
+++ app/controllers/spree/admin/reports_controller_decorator.rb
@@ -3,7 +3,7 @@ Spree::Admin::ReportsController.class_eval do
   add_available_report! :stock_requests
 
   def stock_requests
-    @search = Spree::StockRequest.ransack(params[:search])
+    @search = Spree::StockRequest.ransack(params[:q])
     @stock_requests = @search.result
   end
 
