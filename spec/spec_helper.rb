b225494892d8f9dfbdf25b643552959b07345ec0
Diego Domingues <diego.domingues16@gmail.com>
2015-04-02 12:36:00 -0300

    Changing analysis of test coverage to code climate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,6 @@
 # Run Coverage report
-require 'simplecov'
-SimpleCov.start do
-  add_filter 'spec/dummy'
-  add_group 'Controllers', 'app/controllers'
-  add_group 'Helpers', 'app/helpers'
-  add_group 'Mailers', 'app/mailers'
-  add_group 'Models', 'app/models'
-  add_group 'Views', 'app/views'
-  add_group 'Libraries', 'lib'
-end
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 
 # Configure Rails Environment
 ENV['RAILS_ENV'] = 'test'
